540c9d15ab5958b8b4bf58c23da61b5069d3cd52
sunny73cr <sunny73cr@protonmail.com>
2020-10-14 16:36:23 +1300

    Fixing directory declaration

--- src/JS/Header.js
+++ src/JS/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './CSS/App.css';
 import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import logo from './Images/index.png';
